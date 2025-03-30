2f5184e795e1286f7f570723def56fb0cb5c998b
EugeneyCh <eugeney@ua.fm>
2025-03-30 12:46:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-const message = 'Welcome to Node.js';
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
