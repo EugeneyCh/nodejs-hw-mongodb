75e58f2740ccf42a960db35eb7e77ca89aeb4e74
EugeneyCh <eugeney@ua.fm>
2025-03-30 15:54:58 +0300

    VI-Day

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import express from 'express';
+import { initMongoDB } from './db/initMongoDB.js';
+import { startServer } from './server.js';
 
-const app = express();
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+bootstrap();
