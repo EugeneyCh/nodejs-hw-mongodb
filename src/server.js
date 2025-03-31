8e4e2fbde7c5626e523f33e5af6610d1fb562e1d
EugeneyCh <eugeney@ua.fm>
2025-03-31 18:23:28 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ export const startServer = () => {
     }),
   );
 
-  app.get('/api/contacts', async (req, res) => {
+  app.get('/contacts', async (req, res) => {
     const data = await getContacts();
 
     res.json({
@@ -34,7 +34,7 @@ export const startServer = () => {
     });
   });
 
-  app.get('/api/contacts/:id', async (req, res) => {
+  app.get('/contacts/:id', async (req, res) => {
     const { id } = req.params;
 
     const data = await getContactsById(id);
