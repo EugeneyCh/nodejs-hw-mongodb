4b6b2623cfc0f74fde0d78e45f2ffadca8eae41c
EugeneyCh <eugeney@ua.fm>
2025-04-12 20:16:53 +0300

    V-Day-Validation

--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -1,4 +1,4 @@
-export const parsePaginationParams = ({ page, perPage }) => {
-  const parsedPage = parseNumber(page, 1);
-const parsePerPage(parsedValue)=parseNumber(perPage,10)
-};
+// export const parsePaginationParams = ({ page, perPage }) => {
+//   const parsedPage = parseNumber(page, 1);
+// const parsePerPage(parsedValue)=parseNumber(perPage,10)
+// };
