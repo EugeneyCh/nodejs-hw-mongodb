f11eae4071c0d5e1376863fb916f330cc3b414cb
EugeneyCh <eugeney@ua.fm>
2025-04-13 12:33:44 +0300

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,14 +1,14 @@
 import Joi from 'joi';
 
 export const contactAddSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().required().min(3).max(30),
   phoneNumber: Joi.string().required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
 export const contactUpdateSchema = Joi.object({
-  name: Joi.string(),
+  name: Joi.string().min(3).max(30),
   phoneNumber: Joi.string(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
