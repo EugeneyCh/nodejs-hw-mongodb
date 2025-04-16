517e5448b30d11ea1e0beb874190fdac9e7f018c
EugeneyCh <eugeney@ua.fm>
2025-04-16 18:49:13 +0300

    IX-Day-Validation

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,14 +2,18 @@ import Joi from 'joi';
 import { contactType } from '../constants/contacts.js';
 
 export const contactAddSchema = Joi.object({
-  name: Joi.string().required().min(3).max(20),
+  name: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
   phoneNumber: Joi.string().required(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid(...contactType),
+  contactType: Joi.string()
+    .valid(...contactType)
+    .required(),
 });
 
 export const contactUpdateSchema = Joi.object({
   name: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
   phoneNumber: Joi.string(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...contactType),
