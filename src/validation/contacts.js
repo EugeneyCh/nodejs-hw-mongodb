903cfcb0413e19f11a5ec74978202f6f97b34375
EugeneyCh <eugeney@ua.fm>
2025-04-17 19:03:14 +0300

    X-Day-Validation

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,7 +3,7 @@ import { contactType } from '../constants/contacts.js';
 
 export const contactAddSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email(),
   phoneNumber: Joi.string().required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string()
