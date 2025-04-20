9acac2284d7cf405ba9dbb6a83ef6dcf15dba3e4
EugeneyCh <eugeney@ua.fm>
2025-04-20 14:27:44 +0300

    III-Day_Auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,21 @@
-import { registerUser, loginUser } from '../services/auth.js';
+import {
+  registerUser,
+  loginUser,
+  refreshUser,
+  logoutUser,
+} from '../services/auth.js';
+
+const setupSession = (res, session) => {
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+};
 
 export const registerController = async (req, res) => {
   await registerUser(req.body);
@@ -12,16 +29,7 @@ export const registerController = async (req, res) => {
 export const loginController = async (req, res) => {
   const session = await loginUser(req.body);
 
-  res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    expires: session.refreshTokenValidUntil,
-  });
-
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    expires: session.refreshTokenValidUntil,
-  });
-
+  setupSession(res, session);
   res.json({
     status: 200,
     message: 'Successfully logged in an user!',
@@ -29,4 +37,29 @@ export const loginController = async (req, res) => {
       accessToken: session.accessToken,
     },
   });
+};
+
+export const refreshController = async (req, res) => {
+  const session = await refreshUser(req.cookies);
+
+  setupSession(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Session successfully refresh!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
+
+export const logoutController = async (req, res) => {
+  if (req.cookies.sessionId) {
+    await logoutUser(req.cookies.sessionId);
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import { logger } from './middlewares/logger.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
@@ -16,6 +17,7 @@ export const startServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
   // app.use(logger);
 
   app.use('/auth', authRouter);
