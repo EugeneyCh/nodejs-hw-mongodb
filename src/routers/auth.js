85b9e33e159ac192b3d858d5223901390794985f
EugeneyCh <eugeney@ua.fm>
2025-04-20 13:21:04 +0300

    III-Day_Auth

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,10 +4,22 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 import { validateBody } from '../utils/validateBody.js';
 
-import { authRegisterSchema } from '../validation/auth.js';
+import { authRegisterSchema, authLoginSchema } from '../validation/auth.js';
+
+import { registerController, loginController } from '../controllers/auth.js';
 
 const authRouter = Router();
 
-authRouter.post('/register', validateBody(authRegisterSchema));
+authRouter.post(
+  '/register',
+  validateBody(authRegisterSchema),
+  ctrlWrapper(registerController),
+);
+
+authRouter.post(
+  '/login',
+  validateBody(authLoginSchema),
+  ctrlWrapper(loginController),
+);
 
 export default authRouter;
